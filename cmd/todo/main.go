30b1337a1fac920f1947c46cab27a8cfd1a1944d
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-11-19 18:33:19 +0530

    testing of adding a todo done made JSON succefully to understand how and why ?

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,41 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	todo "github.com/himanshu1221/Todo_Cli"
+)
+
 const (
 	todoFile = ".todo.json"
 )
 
 func main() {
 
+	add := flag.Bool("add", false, "add a new todo")
+
+	flag.Parse()
+
+	//pointing to package todo that we have created ie todo.go
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample Todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "Invalid Command")
+		os.Exit(0)
+	}
 }
