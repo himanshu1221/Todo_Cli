ff3366d86b1ffc67af4e124443effd8b6b678503
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-11-24 18:36:48 +0530

    deleting todo done

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -16,6 +16,7 @@ func main() {
 
 	add := flag.Bool("add", false, "add a new todo")
 	complete := flag.Int("complete", 0, "Mark a ToDo as completed")
+	del := flag.Int("del", 0, "Delete a todo")
 	flag.Parse()
 
 	//pointing to package todo that we have created ie todo.go
@@ -45,6 +46,18 @@ func main() {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
 	default:
 		fmt.Fprintln(os.Stdout, "Invalid Command")
 		os.Exit(0)
